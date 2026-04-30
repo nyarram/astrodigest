6a3fdd591ab13e2bb35c8ad01b123e215f0990dc
Nikhil <yarramnikhil@gmail.com>
2026-04-29 23:06:09 -0500

    fix(ingestion): correct ESO and ALMA feed URLs, fix HTML entity decoding

--- packages/ingestion/src/parsers/alma.ts
+++ packages/ingestion/src/parsers/alma.ts
@@ -1,7 +1,7 @@
 import type { RawContentInsert } from '@astrodigest/shared'
 import { parseRssFeed } from '../utils/rss.js'
 
-const ALMA_RSS_URL = 'https://www.almaobservatory.org/feed/'
+const ALMA_RSS_URL = 'https://www.almaobservatory.org/en/feed/'
 
 export async function fetchAlma(): Promise<RawContentInsert[]> {
   return parseRssFeed(ALMA_RSS_URL, 'alma')
